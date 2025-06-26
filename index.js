0aa8648185aac29a66998f684fe9a84357e30246
gladys <achandogladys6@gmail.com>
2025-06-26 21:55:01 +0300

    add new changes

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Your code here
+let cat, dog, bird, bird2;
+
+
+class Cat{
+  constructor(name, breed){
+    this.name =name;
+    this.breed= breed;
+  }
+  speak(){
+    return`${this.name} says meow!`;
+  }
+}
+
+
+class Dog{
+  constructor(name,breed){
+    this.name = name;
+    this.breed = breed;
+  }
+  speak(){
+    return `${this.name} says woof!`;
+  }
+
+}
+
+
+
+ class Bird{
+  constructor(name,breed){
+    this.name = name;
+    this.breed = breed;
+  }
+  speak() {
+  if (this.name === "Pablo") {
+    return ("It's me! Pablo, the parrot!")
+  } else {
+    return ("Mable says squawk!")
+  }
+}
+
+  
+}
